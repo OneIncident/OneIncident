723665e55132d6ed0c28345ba939dd8cdde3d493
Garnett Grant <net_grant@hotmail.com>
2022-11-18 00:34:01 -0500

    Boilerplate v1

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BasePageComponent } from './partials/base-page/base-page.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { HeaderComponent } from './partials/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    BasePageComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
